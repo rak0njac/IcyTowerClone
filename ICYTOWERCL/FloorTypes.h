9c97e49de44a31aa4d0197f53d737d451221cbc3
rak0njac <marko.rakonjac98@gmail.com>
2022-08-24 17:47:14 +0200

    reorder floortype intrects

--- ICYTOWERCL/FloorTypes.h
+++ ICYTOWERCL/FloorTypes.h
@@ -5,36 +5,43 @@ class FloorTypes
 {
 private:
 	static sf::IntRect SnowX;
-	static sf::IntRect IceX;
-	static sf::IntRect WoodX;
-	static sf::IntRect BlueX;
-	static sf::IntRect PinkGooX;
-	static sf::IntRect BoneX;
-	static sf::IntRect LianaX;
-	static sf::IntRect MarioX;
-	static sf::IntRect CloudX;
-	static sf::IntRect RainbowX;
-
 	static sf::IntRect SnowY;
-	static sf::IntRect IceY;
-	static sf::IntRect WoodY;
-	static sf::IntRect BlueY;
-	static sf::IntRect PinkGooY;
-	static sf::IntRect BoneY;
-	static sf::IntRect LianaY;
-	static sf::IntRect MarioY;
-	static sf::IntRect CloudY;
-	static sf::IntRect RainbowY;
-
 	static sf::IntRect SnowZ;
+
+	static sf::IntRect IceX;
+	static sf::IntRect IceY;
 	static sf::IntRect IceZ;
+
+	static sf::IntRect WoodX;
+	static sf::IntRect WoodY;
 	static sf::IntRect WoodZ;
+
+	static sf::IntRect BlueX;
+	static sf::IntRect BlueY;
 	static sf::IntRect BlueZ;
+
+	static sf::IntRect PinkGooX;
+	static sf::IntRect PinkGooY;
 	static sf::IntRect PinkGooZ;
+
+	static sf::IntRect BoneX;
+	static sf::IntRect BoneY;
 	static sf::IntRect BoneZ;
+
+	static sf::IntRect LianaX;
+	static sf::IntRect LianaY;
 	static sf::IntRect LianaZ;
+
+	static sf::IntRect MarioX;
+	static sf::IntRect MarioY;
 	static sf::IntRect MarioZ;
+
+	static sf::IntRect CloudX;
+	static sf::IntRect CloudY;
 	static sf::IntRect CloudZ;
+
+	static sf::IntRect RainbowX;
+	static sf::IntRect RainbowY;
 	static sf::IntRect RainbowZ;
 
 public:
